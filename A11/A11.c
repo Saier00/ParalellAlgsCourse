29deea3cb4cfa976ab579b84b5b5d1b9ce1b7601
Saier00 <Olegec35@gmail.com>
2021-12-26 06:59:50 +0300

    Assignment 11 have been added

--- A11/A11.c
+++ A11/A11.c
@@ -1,27 +1,25 @@
 #include <iostream>
 #include "mpi.h"
-#include <iostream>
+using namespace std;
 int main(int argc, char **argv)
 {
 	int rank, size, prev, next;
 	int buf[2];
 	MPI_Init(&argc, &argv);
-	MPI_Request reqs[4];
-	MPI_Status stats[4];
+	MPI_Status stats;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 	prev = rank - 1;
 	next = rank + 1;
 	if (rank == 0) prev = size - 1;
 	if (rank == size - 1) next = 0;
-	MPI_Irecv(&buf[0], 1, MPI_INT, prev, 5, MPI_COMM_WORLD, &reqs[0]);
-	MPI_Irecv(&buf[1], 1, MPI_INT, next, 6, MPI_COMM_WORLD, &reqs[1]);
-	MPI_Isend(&rank, 1, MPI_INT, prev, 6, MPI_COMM_WORLD, &reqs[2]);
-	MPI_Isend(&rank, 1, MPI_INT, next, 5, MPI_COMM_WORLD, &reqs[3]);
-	MPI_Waitall(4, reqs, stats);
+
+	MPI_Sendrecv(&rank, 1, MPI_INT, next, 0, &buf[0], 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &stats);
+	MPI_Sendrecv(&rank, 1, MPI_INT, prev, 0, &buf[1], 1, MPI_INT, next, 0, MPI_COMM_WORLD, &stats);
 	
 	//Your code here.
 	//Here you need to display the number of the current process, and what it receives from the previous and next processes.
-	cout<<"Process rank: "<<rank<<" | Previous: "<<buff[0]<<" | Next: "<<buff[1]<<"\n";
+	cout<<"Process rank: "<<rank<<" | Previous: "<<buf[0]<<" | Next: "<<buf[1]<<"\n";
 	MPI_Finalize();
 }
